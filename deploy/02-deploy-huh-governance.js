ac0b93d641e7efc5f62c32cec397ee1ddf68eaed
DsAtHuH <ds@huh.social>
2022-03-09 16:30:37 +0000

    :fire: Remove unnecessary code.

--- deploy/02-deploy-huh-governance.js
+++ deploy/02-deploy-huh-governance.js
@@ -1,17 +1,11 @@
 import { ethers } from 'hardhat'
 
 const func = async (hre) => {
-  // eslint-disable-next-line no-undef
   const { deploy } = deployments
-  // eslint-disable-next-line no-undef
   const { deployer, proxy01Owner } = await getNamedAccounts()
-  // eslint-disable-next-line no-undef
   const timestampContract = await deployments.get('Timestamp')
-  // eslint-disable-next-line no-undef
   const timestamp = await ethers.getContractAt('Timestamp', timestampContract.address)
-  // eslint-disable-next-line no-undef
   const tokenContract = await deployments.get('Token')
-  // eslint-disable-next-line no-undef
   const SafeERC20 = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol:SafeERC20')
   const safeERC20 = SafeERC20.attach(tokenContract.address)
 
--- deploy/03-upgrade-huh-governance.js
+++ deploy/03-upgrade-huh-governance.js
@@ -1,25 +1,17 @@
 import { getImplementation } from './../src/getImplementation'
 
 const func = async (hre) => {
-  // eslint-disable-next-line no-undef
   const { deploy } = deployments
-  // eslint-disable-next-line no-undef
   const { proxy01Owner } = await getNamedAccounts()
-  // eslint-disable-next-line no-undef
   const timestampContract = await deployments.get('Timestamp')
-  // eslint-disable-next-line no-undef
   const timestamp = await ethers.getContractAt('Timestamp', timestampContract.address)
-  // eslint-disable-next-line no-undef
   const tokenContract = await deployments.get('Token')
-  // eslint-disable-next-line no-undef
   const SafeERC20 = await ethers.getContractFactory('@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol:SafeERC20')
   const safeERC20 = SafeERC20.attach(tokenContract.address)
   const huhGovernance = await ethers.getContractAt('HUHGovernance', (await deployments.get('HUHGovernance')).address)
 
   const previousImplementation = await getImplementation(huhGovernance)
   console.log(`Previous implementation: ${previousImplementation}`)
-  // const implAddress = ethers.utils.hexStripZeros(implHex);
-  // console.log(implAddress);
 
   await deploy('HUHGovernance', {
     contract: 'HUHGovernance_V2',
