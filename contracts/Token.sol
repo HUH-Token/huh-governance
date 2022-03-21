160a883b87f80b0ca65379d0c7e64d62c700b5de
DsAtHuH <ds@huh.social>
2022-03-21 11:37:16 +0000

    :white_check_mark: Locked compiler version accordingly to HUH-03 in Certik audit report.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,7 +2,7 @@
 // The line above is recommended and let you define the license of your contract
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 
 // This is the main building block for smart contracts.
